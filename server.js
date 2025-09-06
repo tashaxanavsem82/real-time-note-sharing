2a7b9008a70f1433743d6b52aedffb0b8c4cbad9
tashaxanavsem82 <swgwull@hotmail.com>
2025-09-06 13:58:58 -0500

    Fix minor typo in console log message for clarity.

--- server.js
+++ server.js
@@ -25,5 +25,5 @@ io.on('connection', (socket) => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}.`);
 });
